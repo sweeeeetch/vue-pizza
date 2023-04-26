7f21acb3eb380a687faf7304baaa6996ecdc8e28
Ivan Sinyakov <bglaze@mail.ru>
2023-04-26 21:58:21 +1000

    fixed minor issues, reorganized type for cart item

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Cart from "../views/Cart.vue";
-import NotFound from "../views/notFound.vue";
+import NotFound from "../views/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
